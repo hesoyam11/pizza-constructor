3e48310dd6fcf1540cad44c211a7b21a4095aa45
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-03-28 21:27:58 +0200

    enabled `MinValueValidator` for `price` field in `Ingredient` model

--- constructor/models.py
+++ constructor/models.py
@@ -1,4 +1,6 @@
+from decimal import Decimal
 from django.db import models
+from django.core.validators import MinValueValidator
 
 
 class IngredientGroup(models.Model):
@@ -15,7 +17,8 @@ class Ingredient(models.Model):
         on_delete=models.CASCADE
     )
     name = models.CharField(max_length=128)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2,
+                                validators=[MinValueValidator(Decimal('0.01'))])
 
     def __str__(self):
         return self.name
