2b2dd8ee2f3e67520403ad7a3ac3663a62d337b2
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-03-29 17:36:21 +0200

    displayed groups of ingredients and ingredients in `pizza_constructor` template

--- constructor/views.py
+++ constructor/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import IngredientGroup
 
 
 def pizza_construct(request):
+    if request.method == 'POST':
+        print('POST detected!')
+
+    groups = IngredientGroup.objects.all()
+
     return render(
         request,
         'constructor/pizza_constructor.html',
         {
-
+            'groups': groups
         }
     )
