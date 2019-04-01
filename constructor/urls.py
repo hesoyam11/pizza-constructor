0bdbd53b6d59eadceb95b65aeb4163f3e6c099c7
Vadym Kaidalov <vadym.kaidalov@gmail.com>
2019-04-01 13:49:00 +0300

    added an order confirmation email sending and link confirmation

--- constructor/urls.py
+++ constructor/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.pizza_construct,
-         name='pizza_construct')
+    path('', views.pizza_construct, name='pizza_construct'),
+    path(
+        'order/<int:order_id>/confirm/<slug:token>/',
+        views.order_confirm, name='order_confirm'
+    )
 ]
--- constructor/views.py
+++ constructor/views.py
@@ -1,8 +1,15 @@
 from decimal import Decimal
-from django.shortcuts import render
-from django.http import HttpResponseBadRequest
-from .models import IngredientGroup, Ingredient, IngredientAmount
+from django.contrib.sites.shortcuts import get_current_site
+from django.core import mail
+from django.shortcuts import render, get_object_or_404
+from django.template.loader import render_to_string
+from django.http import HttpResponseBadRequest, HttpResponse, HttpResponseServerError
+from django.utils import timezone
+from django.utils.html import strip_tags
+from smtplib import SMTPAuthenticationError
+from .models import IngredientGroup, Ingredient, IngredientAmount, PizzaOrder
 from .forms import PizzaOrderForm
+from pizza_constructor.local_settings import EMAIL_HOST_USER
 
 
 def pizza_construct(request):
@@ -49,6 +56,30 @@ def pizza_construct(request):
             )
 
         pizza_order = pizza_order_form.save()
+
+        current_site = get_current_site(request)
+        mail_subject = 'Confirm your pizza order.'
+
+        html_message = render_to_string('constructor/confirmation_letter.html', {
+            'pizza_order': pizza_order,
+            'domain': current_site.domain,
+        })
+        plain_message = strip_tags(html_message)
+
+        try:
+            mail.send_mail(
+                mail_subject,
+                plain_message,
+                f'Pizza Constructor { EMAIL_HOST_USER }',
+                [pizza_order.email],
+                html_message=html_message
+            )
+        except SMTPAuthenticationError:
+            pizza_order.delete()
+            return HttpResponseServerError(
+                content='An error occurred while sending email on the server.'
+            )
+
         for ingredient_amount in amounts:
             ingredient_amount.pizza_order = pizza_order
             ingredient_amount.save()
@@ -68,3 +99,17 @@ def pizza_construct(request):
             'groups': groups
         }
     )
+
+
+def order_confirm(request, order_id, token):
+    pizza_order = get_object_or_404(PizzaOrder, pk=order_id)
+
+    if pizza_order.date_confirmed:
+        return HttpResponse('The order is already confirmed.')
+
+    if pizza_order.confirmation_token != token:
+        return HttpResponseBadRequest('Confirmation link is invalid.')
+
+    pizza_order.date_confirmed = timezone.now()
+    pizza_order.save()
+    return HttpResponse('Thank you for your email order confirmation.')
